d29f7efdb42ec3009f2b610d421c91f990a7074a
KhaledMosaad <khalidmosaad155@gmail.com>
2021-06-13 00:07:41 +0200

    Add project files.

---
+++ ITI.Car.Services/CarService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Car.Services
+{
+    public class CarService : ICarService
+    {
+        private static readonly List<Car> Cars = new List<Car>
+        {
+            new Car{Id=1,Model="Fiat",Price=8000},
+            new Car{Id=2,Model="BMW",Price=50000}
+        };
+        public Car Add(Car car)
+        {
+            Cars.Add(car);
+            return car;
+        }
+
+        public List<Car> GetAll()
+        {
+            return Cars;
+        }
+
+        public Car GetbyId(string id)
+        {
+            return Cars.FirstOrDefault(c => c.Id == int.Parse(id));
+        }
+
+        public List<Car> GetbyModel(string model)
+        {
+            return Cars.Where(c => c.Model == model).ToList();
+        }
+
+        public Car Modify(Car car)
+        {
+            Cars.RemoveAll(c => c.Id == car.Id);
+            Cars.Add(car);
+            return car;
+        }
+
+        public void Remove(string id)
+        {
+            Cars.RemoveAll(c => c.Id == int.Parse(id));
+        }
+    }
+}
---
+++ ITI.Car.Services/ICarService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Car.Services
+{
+    [ServiceContract]
+    public interface ICarService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate="Car/Id/{id}")]
+        Car GetbyId(string id);
+        [OperationContract]
+        [WebGet(UriTemplate="Car/Model/{model}")]
+        List<Car> GetbyModel(string model);
+        [OperationContract]
+        [WebInvoke(Method ="PUT",UriTemplate ="Car")]
+        Car Add(Car car);
+        [OperationContract]
+        [WebInvoke(Method ="POST",UriTemplate ="Car")]
+        Car Modify(Car car);
+        [OperationContract]
+        [WebGet(UriTemplate="Cars")]
+        List<Car> GetAll();
+        [OperationContract]
+        [WebInvoke(Method ="DELETE",UriTemplate ="Car/{id}")]
+        void Remove(string id);
+    }
+}
